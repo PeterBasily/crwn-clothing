00a156589f6582a9403ab03c59b523eeeed4cfa6
Peter <baspete49@gmail.com>
2021-09-20 17:45:37 -0400

    make sure everything is updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/homepage.component'
-function App() {
-  return (
-    <div>
-     <HomePage />
-    </div>
-  );
+
+ class App extends React.Component {
+   constructor(){
+     super();
+   }
+   render(){
+    return (
+      <div>
+      <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/homepage.component.jsx
+++ src/components/homepage.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './homepage.styles.scss';
 
 const HomePage = () => (
     
     <div className = 'homepage'>
+        <h1>Welcome to my Homepage</h1>
         <div className='directory-menu'>
             <div className = 'menu-item'>
                 <div className='content'>
@@ -10,32 +12,32 @@ const HomePage = () => (
                     <span className = 'subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
+        
+        
             <div className = 'menu-item'>
                 <div className='content'>
                     <h1 className = 'title'>JACKETS</h1>
                     <span className = 'subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
+        
+        
             <div className = 'menu-item'>
                 <div className='content'>
                     <h1 className = 'title'>SNEAKERS</h1>
                     <span className = 'subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
+        
+        
             <div className = 'menu-item'>
                 <div className='content'>
                     <h1 className = 'title'>WOMENS</h1>
                     <span className = 'subtitle'>SHOP NOW</span>
                 </div>
             </div>
-        </div>
-        <div className='directory-menu'>
+        
+        
             <div className = 'menu-item'>
                 <div className='content'>
                     <h1 className = 'title'>MENS</h1>
@@ -44,6 +46,8 @@ const HomePage = () => (
             </div>
         </div>
     </div>
+        
+    
      
 )
 export default HomePage;
